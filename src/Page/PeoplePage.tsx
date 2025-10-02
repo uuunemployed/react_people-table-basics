4fb4c937b44dead3193156ae8b6b3d270a0695c0
Ivan Denys <denisivan06007@gmail.com>
2025-10-02 20:27:17 +0300

    task solution

--- src/Page/PeoplePage.tsx
+++ src/Page/PeoplePage.tsx
@@ -2,7 +2,7 @@ import { Loader } from '../components/Loader';
 import { getPeople } from '../api';
 import { useEffect, useState } from 'react';
 import { Person } from '../types';
-import { PeopleList } from '../components/PeopleList/PeopleList';
+import { PeopleTable } from '../components/PeopleTable/PeopleTable';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
@@ -40,7 +40,7 @@ export const PeoplePage = () => {
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
 
-          {people.length !== 0 && <PeopleList people={people} />}
+          {people.length !== 0 && <PeopleTable people={people} />}
         </div>
       </div>
     </>
